644e80abd0fac9b632b47c3de9a12795b5a75687
PauloWgDev <paulowaimberg@gmail.com>
2025-08-03 18:26:03 +0800

    Rate Filetering Service

--- src/main/java/com/diversestudio/unityapi/service/RateLimiterService.java
+++ src/main/java/com/diversestudio/unityapi/service/RateLimiterService.java
@@ -1,6 +1,32 @@
 package com.diversestudio.unityapi.service;
 
 
+import io.github.bucket4j.*;
+import io.github.bucket4j.local.LocalBucket;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
 public class RateLimiterService {
 
+    private final Map<String, Bucket> cache = new ConcurrentHashMap<>();
+
+    public Bucket resolveBucket(String key) {
+        return cache.computeIfAbsent(key, k -> newBucket());
+    }
+
+    private  Bucket newBucket() {
+        Refill refill = Refill.greedy(5, Duration.ofMinutes(1));
+        Bandwidth limit = Bandwidth.classic(5, refill);
+        BucketConfiguration config = BucketConfiguration.builder()
+                .addLimit(limit)
+                .build();
+
+        return Bucket.builder()
+                .addLimit(limit)
+                .build();
+    }
 }
