6d88a9f89cfefc9d86126a432b577ec7af65dd40
PauloWgDev <paulowaimberg@gmail.com>
2025-10-25 12:04:23 +0800

    Update Docs, spring boot power plant svg

--- docs/src/components/HomepageFeatures/index.tsx
+++ docs/src/components/HomepageFeatures/index.tsx
@@ -1,52 +1,52 @@
-import type {ReactNode} from 'react';
-import clsx from 'clsx';
-import Heading from '@theme/Heading';
-import styles from './styles.module.css';
+import type { ReactNode } from "react";
+import clsx from "clsx";
+import Heading from "@theme/Heading";
+import styles from "./styles.module.css";
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>> | null;
+  Svg: React.ComponentType<React.ComponentProps<"svg">> | null;
   description: ReactNode;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Full Control for Developers',
-    Svg: require('@site/static/img/logo3.svg').default,
+    title: "Full Control for Developers",
+    Svg: require("@site/static/img/logo3.svg").default,
     description: (
       <>
-        OpenUGC is built to give you maximum flexibility. 
-        You decide how and where to host, freely adapt the codebase,
-        and shape UGC to fit your project’s vision.
+        OpenUGC is built to give you maximum flexibility. You decide how and
+        where to host, freely adapt the codebase, and shape UGC to fit your
+        project’s vision.
       </>
     ),
   },
   {
-    title: 'Powered by Java SpringBoot',
-    Svg: require('@site/static/img/logo3.svg').default,
+    title: "Powered by Java SpringBoot",
+    Svg: require("@site/static/img/PowerPlant.svg").default,
     description: (
       <>
-        Powered by Java Spring Boot, the backend is modular, scalable,
-        and easy to extend. Add custom features or integrations with ease. 
+        Powered by Java Spring Boot, the backend is modular, scalable, and easy
+        to extend. Add custom features or integrations with ease.
       </>
     ),
   },
   {
-    title: 'Unity Integration in Minutes',
-    Svg: require('@site/static/img/logo3.svg').default,
+    title: "Unity Integration in Minutes",
+    Svg: require("@site/static/img/logo3.svg").default,
     description: (
       <>
-        With <b>U3GC</b>, our Unity client library, you can bring
-        UGC into your game in just a few lines of code—fast setup,
-        smooth integration, and ready to scale.
+        With <b>U3GC</b>, our Unity client library, you can bring UGC into your
+        game in just a few lines of code—fast setup, smooth integration, and
+        ready to scale.
       </>
     ),
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, Svg, description }: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         {Svg && <Svg className={styles.featureSvg} role="img" />}
       </div>
